3662a598629ae7b29fbc7f1d19a69f514abb38a1
Appwrk <sahilghangash950@gmail.com>
2022-10-14 15:42:14 +0530

    making it better

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,31 +3,29 @@ import User from "../models/userModel.js";
 
 export const createChat = async (req, res) => {
   const { currentUserId, otherUserId } = req.body;
-  console.log(req.body);
   const users = [currentUserId, otherUserId];
-  const reverseUsers = [currentUserId, otherUserId];
 
   try {
-    const isCurrentUserIdCorrect = await User.find({
-      user: currentUserId,
-    }).count();
-    const isOtherUserIdCorrect = await User.find({ user: otherUserId }).count();
+    // const isCurrentUserIdCorrect = await User.find({
+    //   user: currentUserId,
+    // }).count();
+    // const isOtherUserIdCorrect = await User.find({ user: otherUserId }).count();
 
-    if (isCurrentUserIdCorrect < 1)
-      return res.status(200).send("current user id incorrect 1");
-    if (isOtherUserIdCorrect < 1)
-      return res.status(200).send("other user id incorrect 2");
+    // if (isCurrentUserIdCorrect < 1)
+    //   return res.status(200).send("current user id incorrect 1");
+    // if (isOtherUserIdCorrect < 1)
+    //   return res.status(200).send("other user id incorrect 2");
 
-    const isExistBefore = await Chat.findOne({
-      users,
+    var isExistBefore = await Chat.find({
+      $and: [
+        { users: { $elemMatch: { $eq: "63491222d73c780000bfa9f7" } } },
+        { users: { $elemMatch: { $eq: "634929feae8a880000c41437" } } },
+      ],
     });
-    const isExistBefore2 = await Chat.findOne({
-      reverseUsers,
-    });
-    console.log(isExistBefore);
 
-    if (isExistBefore !== null && isExistBefore2 !== null)
+    if (isExistBefore) {
       return res.status(200).json(isExistBefore);
+    }
 
     const newChat = new Chat({ users });
     await newChat.save();
